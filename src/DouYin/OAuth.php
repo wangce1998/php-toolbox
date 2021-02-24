1224c8c348c8c1135d328408d8316b61da207839
王策 <wangce@vchangyi.com>
2021-02-24 22:10:51 +0800

    增加接口

--- src/DouYin/OAuth.php
+++ src/DouYin/OAuth.php
@@ -14,9 +14,15 @@ class OAuth
      */
     protected $config;
 
+    /**
+     * @var Client
+     */
+    protected $client;
+
     public function __construct(Config $config)
     {
         $this->config = $config;
+        $this->client = new Client();
     }
 
     /**
@@ -24,7 +30,7 @@ class OAuth
      *
      * @param string $code
      * @return array
-     * @throws Exception
+     * @throws \GuzzleHttp\Exception\GuzzleException|Exception
      */
     public function accessToken(string $code): array
     {
@@ -37,12 +43,50 @@ class OAuth
         $url = Config::DOMAIN . '/oauth/access_token?' . http_build_query($params);
         $response = $this->request('GET', $url);
         $content = $response->getBody()->getContents();
-        $contentArr = Json::decode($content);
-        if (is_array($contentArr) === false) {
-            throw new Exception(Config::NAME . '服务解析错误');
-        }
 
-        return $contentArr;
+        return $this->decodeContent($content);
+    }
+
+    /**
+     * 获取access token
+     *
+     * @param string $refreshToken
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException|Exception
+     */
+    public function refreshToken(string $refreshToken): array
+    {
+        $params = [
+            'client_key'    => $this->config->clientKey,
+            'refresh_token' => $refreshToken,
+            'grant_type'    => 'refresh_token'
+        ];
+        $url = Config::DOMAIN . '/oauth/refresh_token?' . http_build_query($params);
+        $response = $this->request('GET', $url);
+        $content = $response->getBody()->getContents();
+
+        return $this->decodeContent($content);
+    }
+
+    /**
+     * 获取用户信息
+     *
+     * @param string $openid
+     * @param string $accessToken
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException|Exception
+     */
+    public function userinfo(string $openid, string $accessToken): array
+    {
+        $params = [
+            'open_id'      => $openid,
+            'access_token' => $accessToken
+        ];
+        $url = Config::DOMAIN . '/oauth/userinfo?' . http_build_query($params);
+        $response = $this->request('GET', $url);
+        $content = $response->getBody()->getContents();
+
+        return $this->decodeContent($content);
     }
 
     /**
@@ -56,8 +100,23 @@ class OAuth
      */
     protected function request(string $method, string $url, array $options = []): ResponseInterface
     {
-        $client = new Client();
+        return $this->client->request(strtoupper($method), $url, $options);
+    }
 
-        return $client->request(strtoupper($method), $url, $options);
+    /**
+     * 解析响应内容
+     *
+     * @param string $content
+     * @return array
+     * @throws Exception
+     */
+    protected function decodeContent(string $content): array
+    {
+        $contentArr = Json::decode($content);
+        if (is_array($contentArr) === false) {
+            throw new Exception(Config::NAME . '服务解析错误');
+        }
+
+        return $contentArr;
     }
 }
