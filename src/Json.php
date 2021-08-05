84d23c4aa583d547e7e7104d8852d69a814f9d5e
wangce <wangce@localhost.localdomain>
2021-08-05 11:50:53 +0800

    新增

--- src/Json.php
+++ src/Json.php
@@ -4,7 +4,7 @@ namespace PHPToolbox;
 
 class Json
 {
-    public static function encode($value, $option = JSON_UNESCAPED_UNICODE + JSON_UNESCAPED_SLASHES)
+    public static function encode($value, $option = JSON_UNESCAPED_UNICODE)
     {
         return json_encode($value, $option);
     }
