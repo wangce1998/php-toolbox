6f410ab459ade202f22bba0b2bc5593f82c8557c
wangce <wangce@vchangyi.com>
2021-09-08 18:09:15 +0800

    json支持反斜杠

--- src/Json.php
+++ src/Json.php
@@ -4,7 +4,7 @@ namespace PHPToolbox;
 
 class Json
 {
-    public static function encode($value, $option = JSON_UNESCAPED_UNICODE)
+    public static function encode($value, $option = JSON_UNESCAPED_UNICODE + JSON_UNESCAPED_SLASHES)
     {
         return json_encode($value, $option);
     }
